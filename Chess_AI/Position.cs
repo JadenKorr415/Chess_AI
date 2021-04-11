99f3370a8b8653b4f8b32438ec7e770134f3caae
JadenKorr415 <jaden.korr415@gmail.com>
2021-04-11 10:30:17 -0600

    Added Position class

---
+++ Chess_AI/Position.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess_AI
+{
+	class Position
+	{
+		protected char column;
+		protected int row;
+
+		public Position()
+		{
+			this.column = 'A';
+			this.row = 1;
+		}
+
+		public Position(char c, int r)
+		{
+			this.column = c;
+			this.row = r;
+		}
+
+		public void setColumn(char c)
+		{
+			this.column = c;
+		}
+
+		public char getColumn()
+		{
+			return this.column;
+		}
+
+		public void setRow(int r)
+		{
+			this.row = r;
+		}
+
+		public int getRow()
+		{
+			return this.row;
+		}
+	}
+}
